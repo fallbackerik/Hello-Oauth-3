6ad548c041e955dd3634708da5df64fa144c9512
Erik <fallbackerik@gmail.com>
2025-03-05 19:39:56 +0100

    add some logging

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const config = {};
 const oidcClient = new Oidc.OidcClient(config);
 document.getElementById("startLogin").addEventListener("click", () => {
+  console.log("startLogin clicked");
   oidcClient.createAuthorizationRequest({}).then(authorizationRequest => {
     window.location = authorizationRequest.url;
   });
+  console.log("auth flow succeeded");
 });
+
+console.log("app.js loaded");
