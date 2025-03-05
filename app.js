d6485a274427930f60d5443a2087d32545d07e5e
Erik <fallbackerik@gmail.com>
2025-03-05 20:09:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const config = {};
+const config = {
+    authority: 'https://accounts.google.com', // e.g., 'https://accounts.google.com' for Google
+    client_id: '960468044348-jh6jlm9rk7u47ejgskfnchc2l1j8i3p6.apps.googleusercontent.com ', // Replace with your Client ID from Google Cloud Console
+    redirect_uri: window.location.origin + '/callback.html', // Callback URL - important for GitHub Pages
+    response_type: 'code',
+    scope: 'openid profile email', // Scopes to request
+    pkce: true, // Enable PKCE
+};
 const oidcClient = new Oidc.OidcClient(config);
 document.getElementById("startLogin").addEventListener("click", () => {
   console.log("startLogin clicked");
